9bc09f5ea80b850cd1f05bd6fc890c08ae4d4f70
pawani2497 <70946097+pawani2497@users.noreply.github.com>
2020-09-27 00:01:32 +0530

    First class successfulness

---
+++ first.py
@@ -0,0 +1,9 @@
+x=input('Whether the class was successful or no\n')
+if x=='yes' or x=='Yes' or x=='YES':
+	for i in range(10):
+		print("Let's meet tomorrow")
+else:
+	n=10
+	while (n>0):
+		print('I am sorry, I tried my best')
+		n-=1
